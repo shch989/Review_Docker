bf8b22f4d6a48818bd3bd6095d941fdfad57e8f4
shch989 <tankpop11@gmail.com>
2023-01-31 15:30:26 +0900

    start docker

---
+++ app.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import connectToDatabase from './helpers.mjs'
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h2>Hi there!</h2>');
+});
+
+await connectToDatabase();
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000/`)
+})
