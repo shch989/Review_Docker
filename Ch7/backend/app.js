df3877916df8dcbdb8cebabd3a1eb0c62a7e6125
shch989 <tankpop11@gmail.com>
2023-02-05 23:04:39 +0900

    마무리

--- Ch7/backend/app.js
+++ Ch7/backend/app.js
@@ -84,7 +84,7 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://mongodb:27017/course-goals',
+  'mongodb://semi:secret@mongodb:27017/course-goals?authSource=admin',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
